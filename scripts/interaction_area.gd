74fbffd403ca84505a2b3ae4cdcc7951ab287984
Norden-Tenzin <nordentenzin29@gmail.com>
2025-02-17 21:28:45 +0530

    factory_job got it working, it should be easily expandable

---
+++ scripts/interaction_area.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+signal hit
+
+func _input(event: InputEvent) -> void:
+    if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+        hit.emit()
+        if has_overlapping_bodies() or has_overlapping_areas():
+            for item in get_overlapping_areas():
+                if item.owner and item.owner.has_method("hit"):
+                    print("Hit Called")
+                    item.owner.hit()
+        else:
+            print("Nothing here")
---
+++ scripts/spawner.gd
@@ -0,0 +1,44 @@
+extends Node2D
+
+enum FactoryObject {
+  Nail,
+  Space
+}
+
+@onready var timer:Timer = $Timer
+
+# demo sequence
+var sequence: Array[FactoryObject] = [
+  FactoryObject.Nail,
+  FactoryObject.Nail,
+  FactoryObject.Space,
+  FactoryObject.Nail,
+  FactoryObject.Space,
+  FactoryObject.Nail,
+  FactoryObject.Space,
+  FactoryObject.Nail,
+  FactoryObject.Space,
+  FactoryObject.Nail,
+]
+
+func _ready() -> void:
+  timer.start()
+
+# on timeout checks if there is something in seq
+# if there is instantiates it and adds that child
+# Note: This dosent handle movement, that should 
+# be in the child node
+func _on_timer_timeout() -> void:
+  const nail = preload("res://scenes/nail.tscn")
+  
+  if sequence.is_empty():
+    timer.stop()
+  else:
+    var currObject: FactoryObject = sequence.pop_front()
+    match currObject:
+      FactoryObject.Nail:
+        var factory_object: Nail = nail.instantiate()
+        add_child(factory_object)
+      _:
+        pass
+  
