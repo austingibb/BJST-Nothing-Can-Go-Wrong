fb53ef632d528aa9db1390751c8a4256b75b5d96
Norden-Tenzin <nordentenzin29@gmail.com>
2025-02-20 15:36:56 +0530

    added movement with placeholder sprites and removed the use of has and get nodes for @export

--- components/highlight_component/highlight_component.gd
+++ components/highlight_component/highlight_component.gd
@@ -1,15 +1,17 @@
 class_name HighlightComponent
 extends Node
 
+@export var sprite: Sprite2D
 @export var shader_material: ShaderMaterial
 
 func highlight(enabled: bool) -> void:
-	if shader_material:
-		if owner.has_node("Sprite2D"):
-			var sprite_node: Sprite2D = owner.get_node("Sprite2D")
-			if enabled:
-				sprite_node.material = shader_material
-			else:
-				sprite_node.material = null
-	else:
-		push_warning("No shader material assigned!")
+  if shader_material:
+    if sprite:
+      if enabled:
+        sprite.material = shader_material
+      else:
+        sprite.material = null
+    else:
+      push_warning("No Sprite2D assigned!")
+  else:
+    push_warning("No shader material assigned!")
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,15 +3,15 @@ var bus_audio : int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	bus_audio = AudioServer.get_bus_index("Master")
-	var value : int = db_to_linear(AudioServer.get_bus_volume_db(bus_audio))
+  bus_audio = AudioServer.get_bus_index("Master")
+  var value : int = db_to_linear(AudioServer.get_bus_volume_db(bus_audio))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+  pass
 
 func _on_h_slider_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(bus_audio, linear_to_db(value / 100))
+  AudioServer.set_bus_volume_db(bus_audio, linear_to_db(value / 100))
 
 func _on_button_pressed() -> void:
-	get_tree().quit()
+  get_tree().quit()
