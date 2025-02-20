a304b5c1d51ed3ad61fb32a21167d24a55bc5aaa
Luca Bison <lucabison.lb@gmail.com>
2025-02-20 10:57:12 +0100

    Asset V2 - main menu (on level_manager)

--- components/controller/controller_component.gd
+++ components/controller/controller_component.gd
@@ -18,9 +18,14 @@ func _process(delta: float) -> void:
 
 # this is the relevant code for handling opening the next level proof of concept
 func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("open_menu"):  # Default "ESC" action in Godot
+		toggle_menu()
 	if event.is_action_pressed("change_level"):
-		_open_next_level()
+		open_next_level()
+		
+func toggle_menu() -> void:
+	LevelManager.toggle_menu()
 
 # this is the relevant code for handling opening the next level proof of concept
-func _open_next_level() -> void:
+func open_next_level() -> void:
 	LevelManager.change_scene(packed_scene.get_path())
