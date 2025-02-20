9f19e38012bdc9a9b67e281ecfdee911d0623a8e
Luca Bison <lucabison.lb@gmail.com>
2025-02-20 21:39:27 +0100

    Persistent and save logic

--- components/persistent.gd
+++ components/persistent.gd
@@ -1,28 +1,8 @@
 extends Node
 class_name Persistent
 
-@export var unique_id: String = ""  
-@export var save_position: bool = true 
-@export var save_state: bool = true
+@export var unique_id: String = ""
+@export var is_interacted: bool = false
 
-var data: Dictionary = {}
-
-func get_save_data() -> Dictionary:
-	data["id"] = unique_id
-	if save_position and owner is Node2D:
-		data["position"] = owner.position
-	if save_state:
-		data["state"] = get_state()
-	return data
-
-func load_from_data(save_data: Dictionary) -> void:
-	if save_position and "position" in save_data:
-		owner.position = save_data["position"]
-	if save_state and "state" in save_data:
-		set_state(save_data["state"])
-
-func get_state() -> Dictionary:
-	return {}
-
-func set_state(state_data: Dictionary) -> void:
-	pass
+func _ready() -> void :
+	add_to_group("Persistent")
