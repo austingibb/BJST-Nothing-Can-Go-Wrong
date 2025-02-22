3a8b5b089b90749bad1f4bf42a912523b1035c08
Norden-Tenzin <nordentenzin29@gmail.com>
2025-02-22 19:01:25 +0530

    added assets to the factory_minigame

--- scripts/interaction_area.gd
+++ scripts/interaction_area.gd
@@ -1,14 +1,26 @@
 extends Area2D
 
+@onready var timer: Timer = $Timer
+var can_hit: bool = true  # Prevents hitting during cooldown
+
 signal hit
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		hit.emit()
-		if has_overlapping_bodies() or has_overlapping_areas():
+		if can_hit and (has_overlapping_bodies() or has_overlapping_areas()):
+			can_hit = false  # Disable further hits
+			hit.emit()
+			%Hit.visible = true
+			%NotHit.visible = false
 			for item in get_overlapping_areas():
 				if item.owner and item.owner.has_method("hit"):
 					print("Hit Called")
 					item.owner.hit()
+			timer.start()  # Start cooldown
 		else:
 			print("Nothing here")
+
+func _on_timer_timeout() -> void:
+	can_hit = true  # Allow hitting again
+	%Hit.visible = false
+	%NotHit.visible = true
