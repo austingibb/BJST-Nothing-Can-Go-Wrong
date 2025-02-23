366fd8a2e15c148ee484b6e203fb8affd28dea44
Norden-Tenzin <nordentenzin29@gmail.com>
2025-02-23 10:06:50 +0530

    fixes

--- scripts/kill_box.gd
+++ scripts/kill_box.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+signal factory_object_killed
+
 func _on_area_entered(area: Area2D) -> void:
 	# kills anything that collides
-	area.owner.queue_free()
+	factory_object_killed.emit()
