5e27716b5d058979ec72fe9e2b4b58d32262b398
Austin G <austingibb@gmail.com>
2025-03-08 09:42:36 -0800

    demo cheat manager capabilities by toggling ball color
    Note: This breaks the outline shader on the ball. I'll merge the color replacement and the outline shader soon to a meta shader that can do both.

--- scripts/god-prophet/apartment_prophet.gd
+++ scripts/god-prophet/apartment_prophet.gd
@@ -1,4 +1,7 @@
 extends Node2D
 
+@export var ballNode : BjstBall 
+
 func _on_cheat_manager_cheat_activated(cheat: String) -> void:
-	print("Prophet received cheat action: ", cheat)
+	if cheat == "change-ball-color" and is_instance_valid(ballNode):
+		ballNode.toggle_color()
--- scripts/items/ball.gd
+++ scripts/items/ball.gd
@@ -1,5 +1,18 @@
 extends Node2D
 
+class_name BjstBall
+
+var shader_material : ShaderMaterial = null
+var enable_ball_color_replacement : bool = false
+
+func _ready() -> void:
+	var ball_sprite2d : Sprite2D = get_node("Sprite2D") as Sprite2D
+	shader_material = ball_sprite2d.material as ShaderMaterial
+
 func _on_interactable_component_interacted() -> void:
 	if Dialogic.current_timeline == null:
 		Dialogic.start("ball")
+
+func toggle_color() -> void:
+	enable_ball_color_replacement = !enable_ball_color_replacement
+	shader_material.set_shader_parameter("enabled", enable_ball_color_replacement)
